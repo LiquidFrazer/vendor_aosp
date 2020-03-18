4143b444c7aee71c2641f75cb5d0af0f70f3b853
SparXFusion <s2234nadar@gmail.com>
2020-03-19 02:12:20 +0530

    Bringup InFusionX-OS

--- config/branding.mk
+++ config/branding.mk
@@ -10,19 +10,17 @@ CUSTOM_BUILD_DATE_UTC := $(shell date -d '$(CUSTOM_DATE_YEAR)-$(CUSTOM_DATE_MONT
 CUSTOM_BUILD_DATE := $(CUSTOM_DATE_YEAR)$(CUSTOM_DATE_MONTH)$(CUSTOM_DATE_DAY)-$(CUSTOM_DATE_HOUR)$(CUSTOM_DATE_MINUTE)
 
 CUSTOM_PLATFORM_VERSION := 10.0
+CUSTOM_BASE_VERSION = 1.0
 
 TARGET_PRODUCT_SHORT := $(subst aosp_,,$(CUSTOM_BUILD))
 
-CUSTOM_VERSION := PixelExperience_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
+CUSTOM_VERSION := InFusionX-OS$(CUSTOM_BASE_VERSION)_$(CUSTOM_BUILD)-$(CUSTOM_PLATFORM_VERSION)-$(CUSTOM_BUILD_DATE)-$(CUSTOM_BUILD_TYPE)
 CUSTOM_VERSION_PROP := 10
 
 CUSTOM_PROPERTIES := \
-    org.pixelexperience.version=$(CUSTOM_VERSION_PROP) \
-    org.pixelexperience.version.display=$(CUSTOM_VERSION) \
-    org.pixelexperience.build_date=$(CUSTOM_BUILD_DATE) \
-    org.pixelexperience.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
-    org.pixelexperience.build_type=$(CUSTOM_BUILD_TYPE)
+    org.infusion.version=$(CUSTOM_VERSION_PROP) \
+    org.infusion.version.display=$(CUSTOM_VERSION) \
+    org.infusion.build_date=$(CUSTOM_BUILD_DATE) \
+    org.infusion.build_date_utc=$(CUSTOM_BUILD_DATE_UTC) \
+    org.infusion.build_type=$(CUSTOM_BUILD_TYPE)
 
-ifeq ($(CUSTOM_BUILD_TYPE), OFFICIAL)
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/secure/releasekey
-endif
