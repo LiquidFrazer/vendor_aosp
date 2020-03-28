b9ad472f606b5a3c9aea43f4a62f7193e9f04880
Sakthivel Nadar <55133980+SparXFusion@users.noreply.github.com>
2020-03-28 15:46:44 +0530

    vendor: fix zip naming

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/$(CUSTOM_VERSION).zip
+CUSTOM_TARGET_PACKAGE := $(PRODUCT_OUT)/$(INFUSION_VERSION).zip
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
